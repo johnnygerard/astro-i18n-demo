c25ba0236f73ada725f793993b46c19e873365f2
Johnny Gérard <john@jgerard.dev>
2026-01-26 19:33:16 +0100

    feat: use root page as language router

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,22 @@
 import type { MiddlewareHandler } from "astro";
-import { getAbsoluteLocaleUrl } from "astro:i18n";
+import { getRelativeLocaleUrl } from "astro:i18n";
 import { DEFAULT_LOCALE } from "~/constants/locales";
 import { isSupportedLocale } from "~/utils/is-supported-locale";
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
-  const response = await next();
+  // If the user requests the root page ("/"), redirect to either
+  // the preferred locale homepage or the language selection page.
+  if (context.url.pathname === "/") {
+    const redirectURL = isSupportedLocale(context.preferredLocale)
+      ? getRelativeLocaleUrl(context.preferredLocale)
+      : "/select-language";
+
+    return context.redirect(redirectURL, 302);
+  }
 
   // If the response status is 404, rewrite to the localized 404 page using either
   // a valid locale URL path prefix, the `accept-language` header or the default locale.
+  const response = await next();
   if (response.status !== 404) return response;
 
   const firstPathSegment = context.url.pathname.split("/")[1];
@@ -22,5 +31,5 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
       ? context.preferredLocale
       : DEFAULT_LOCALE;
 
-  return context.rewrite(getAbsoluteLocaleUrl(targetLocale, "/404"));
+  return context.rewrite(getRelativeLocaleUrl(targetLocale, "/404"));
 };
